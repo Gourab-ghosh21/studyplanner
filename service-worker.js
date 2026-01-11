a84b90512b6943b9a79dd5a68c98a6b4bb8f3902
Gourab Ghosh <ggourab217@gmail.com>
2026-01-11 22:18:38 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,6 @@ const FILES_TO_CACHE = [
   "./script.js",
   "./manifest.json"
 ];
-
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
