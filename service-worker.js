52bee4ca34ef120a93fd720809f4c64d51d2c3db
Gourab Ghosh <ggourab217@gmail.com>
2026-01-11 22:18:50 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,6 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
-
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
